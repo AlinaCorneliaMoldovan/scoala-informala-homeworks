e4c6adf9b828bd64bcb73f48770c37ffdc4556d4
AlinaCorneliaMoldovan <alina_ghiurco@yahoo.com>
2017-08-27 18:41:37 +0300

    Add files via upload

---
+++ java-grupa8/homework-3/Tema3/src/AgentiVanzariFirma.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Alina Moldovan on 7/31/2017.
+ */
+public class AgentiVanzariFirma {
+    //String dep1, dep2, dep3, dep4;
+    String nume;
+    int vanzari;
+    //int sum;
+    int dep;
+    //String ag;
+}
